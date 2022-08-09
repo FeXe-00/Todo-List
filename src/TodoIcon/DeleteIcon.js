440ed707d6164439363f72d344a8716997632ef4
FeXe-00 <fsosaescalada@gmail.com>
2022-08-09 19:02:52 -0300

    primera version todolist

--- src/TodoIcon/DeleteIcon.js
+++ src/TodoIcon/DeleteIcon.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { TodoIcon } from "./";
 
-function DeleteIcon({ onDelete }) {
+function DeleteIcon({onDelete}) {
     return (
         <TodoIcon
             type="delete"
